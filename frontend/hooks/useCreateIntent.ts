d8d798f449fa91e3746837f7f57a7eb1621ff35a
Nayab Khan <nayab4657@gmail.com>
2026-06-02 00:58:30 +0530

    reveal error fixed

--- frontend/hooks/useCreateIntent.ts
+++ frontend/hooks/useCreateIntent.ts
@@ -41,6 +41,9 @@ export function useCreateIntent() {
       functionName: "submitIntent",
 
       args: [commitmentHash, expiry],
+
+      maxFeePerGas: BigInt(50000000),
+      maxPriorityFeePerGas: BigInt(2000000),
     });
   }
 
